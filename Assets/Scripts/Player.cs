a12836ad8f3563b2ccfa65d1c8fe9f7ae18f69b0
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-11-25 21:12:32 -0600

    Added UpdateRunningState(bool) Method to player script. The player sprite now transitions from idle->running->idle based on bool.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
 
-    private Rigidbody2D playerRigidBody;
+    Rigidbody2D playerRigidBody;
+    Animator playerAnimator;
     
     // Start is called before the first frame update
     void Start()
     {
-        //Get reference to player rigidbody
+        //Get reference to player rigidbody and animator
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,15 +30,23 @@ public class Player : MonoBehaviour
 
         Vector2 velocity = new Vector2(controlThrow * runSpeed, playerRigidBody.velocity.y);
         playerRigidBody.velocity = velocity;
-        FlipSprite();
-    }
 
-    private void FlipSprite()
-    {
         //.Epsilon is used because the velocity can never be truly 0 from the controller?
         bool runningHorizontally = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
 
-        if(runningHorizontally)
+        FlipSprite(runningHorizontally);
+        UpdateRunningState(runningHorizontally);
+    }
+
+    private void UpdateRunningState(bool isRunning)
+    {
+        //Run animation if running, not if not
+        playerAnimator.SetBool("Running", isRunning);
+    }
+
+    private void FlipSprite(bool isRunning)
+    {
+        if(isRunning)
         {
             // Left -> -1 | Right -> 1 (Mathf.Sign(playerRigidBody.velocity.x))
             transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x) ,1f);
